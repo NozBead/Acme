e72d400dadb923512ef9df690082cb649e256eb4
Enzo Pain <nozbead@outlook.com>
2024-02-18 18:44:33 +0100

    add dns ovh

--- challenge.sh
+++ challenge.sh
@@ -24,7 +24,8 @@ for auth_url in $(curl "$order_url" | jq -r .authorizations[]) ; do
 
 	key_auth=$(echo -n $token.$thumbprint | sha256sum | cut -d " " -f 1 | xxd -p -r | base64urlencode)
 	echo Waitting DNS $identifier : $key_auth
-	../ovh/add_dns_entry.sh $identifier TXT _acme-challenge $key_auth 60
+	echo Press ENTER when ready.
+	ovh/add_dns_entry.sh $identifier TXT _acme-challenge $key_auth 60
 	read
 
 	nonce=$(./get_new_nonce.sh "$directory")
---
+++ ovh/add_dns_entry.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+endpoint="https://eu.api.ovh.com/v1"
+app=
+secret=
+consumer=
+
+post_data() {
+	url="$1"
+	timestamp=$(date +%s)
+	signature='$1$'$(echo -n "$secret+$consumer+POST+$url+$(cat .body)+$timestamp" | tee .test | sha1sum | cut -d ' ' -f 1)
+
+	curl -v $url \
+		-H "X-Ovh-Application: $app" \
+		-H "X-Ovh-Consumer: $consumer" \
+		-H "X-Ovh-Timestamp: $timestamp" \
+		-H "X-Ovh-Signature: $signature" \
+		--json @.body
+}
+
+cat << EOF | tr -d '\n \t' > .body
+{
+	"fieldType": "$2",
+	"subDomain": "$3",
+	"target": "$4",
+	"ttl": $5
+}
+EOF
+
+post_data "$endpoint/domain/zone/$1/record"
+
+rm .body
+touch .body
+
+post_data "$endpoint/domain/zone/$1/refresh"
